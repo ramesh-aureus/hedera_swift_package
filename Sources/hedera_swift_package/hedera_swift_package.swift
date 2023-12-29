9e89f7b1d18b4e08e3e030778be54a26ad40eb6f
ramesh-aureus <ramesh@aureus.money>
2023-12-29 14:21:38 +0545

    fresh push

--- Sources/hedera_swift_package/hedera_swift_package.swift
+++ Sources/hedera_swift_package/hedera_swift_package.swift
@@ -1,2 +1,21 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+import Foundation
+// Import Hedera SDK if necessary
+import Hedera
+public class HederaManager {
+    
+    public init() {
+        // Initialize any necessary Hedera components
+    }
+
+    public func getAccountBalance(completion: @escaping (Result<Int64, Error>) -> Void) {
+        let client = Client.forTestnet()
+        do {
+            let ab = try await AccountBalanceQuery()
+                .accountId(AccountId("0.0.1001")) // Static account ID
+                .execute(client)
+            completion(.success(ab.balance))
+        } catch {
+            completion(.failure(error))
+        }
+    }
+    }
